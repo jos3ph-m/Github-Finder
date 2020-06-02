06413dafbe11774d9797b7c07cdb2ff90982b32b
Joseph M <jmedina1985@hotmail.com>
2020-06-01 23:34:28 -0700

    Covering conditionals and expressions

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import "./App.css";
 
 class App extends Component {
   render() {
+    const name = "Guacamole Jr.";
+    const loading = false;
+    const showName = true;
+
     return (
-      <div className="App">
-        <h1>Hello from React</h1>
+      <div className="app">
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
       </div>
     );
   }

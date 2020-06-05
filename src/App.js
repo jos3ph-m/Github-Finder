f91048331d209e84a4b87e535b7c0e00d6ae7a42
Joseph M <jmedina1985@hotmail.com>
2020-06-04 21:43:13 -0700

    Fully functioning search, added a clear button, and a bit of destructuring

--- src/App.js
+++ src/App.js
@@ -11,23 +11,32 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
+  // Search Github users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
+    console.log(res);
+    this.setState({ users: res.data.items, loading: false });
+  };
 
-    this.setState({ users: res.data, loading: false });
-  }
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="app">
         <Navbar />
         <div className="container">
-          <Search />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
   state = {
     text: "",
   };
 
+  static propTypes = {
+    switchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
+  };
+
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -19,12 +33,17 @@ export class Search extends Component {
             autoComplete="off"
             onChange={this.onChange}
           />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
         </form>
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-dark btn-block"
-        />
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
